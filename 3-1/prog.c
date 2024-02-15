499fda0dcca93e968a6211e6d145cdf6c73356f2
DeBug758 <nikulin.nikita.work@gmail.com>
2024-02-15 14:04:21 +0600

    Fixed tasks

--- 3-1/prog.c
+++ 3-1/prog.c
@@ -5,7 +5,11 @@
 
 int main() {
     pid_t pid_son, pid_grandson1, pid_grandson2;
-
+	int fd[2];
+	if(pipe(fd) == -1){
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
     pid_son = fork();
     if (pid_son == -1) {
         // Ошибка при создании процесса-сына
@@ -16,31 +20,43 @@ int main() {
         pid_grandson1 = fork();
         if (pid_grandson1 == 0) {
             // Это первый процесс-внук
+			close(fd[0]);
             printf("First grandson is doing something\n");
             sleep(1); // Имитация работы
             printf("First grandson finished\n");
+			write(fd[1], "200", 3);
+			close(fd[1]);
             exit(0);
         }
 
         pid_grandson2 = fork();
         if (pid_grandson2 == 0) {
             // Это второй процесс-внук
+			close(fd[0]);
             printf("Second grandson is doing something\n");
             sleep(1); // Имитация работы
             printf("Second grandson finished\n");
+			write(fd[1], "200", 3);
+			close(fd[1]);
             exit(0);
         }
-
-        // Процесс-сын ожидает завершения обоих процессов-внуков
-        waitpid(pid_grandson1, NULL, 0);
-        waitpid(pid_grandson2, NULL, 0);
         printf("Son process finished\n");
         exit(0);
     } else {
+		close(fd[1]);
         // Это процесс-отец
-        // Отец ожидает завершения процесса-сына
-        waitpid(pid_son, NULL, 0);
+        // Отец ожидает завершения процессов-внуков
+		char buffer[5];
+		read(fd[0], buffer, sizeof(buffer));
+		if(buffer[0] == '2' && buffer[1] == '0' && buffer[2] == '0'){
+			read(fd[0], buffer, sizeof(buffer));
+		}
+		else{
+			perror("pipe");
+			return EXIT_FAILURE;
+		}
         printf("Father process finished\n");
+		close(fd[0]);
     }
 
     return EXIT_SUCCESS;
--- 3-3/prog.c
+++ 3-3/prog.c
@@ -10,15 +10,9 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    int pipefd[2];
-    if (pipe(pipefd) == -1) {
+    int fd[2];
+    if (pipe(fd) == -1) {
         perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-
-    FILE *src = fopen(argv[1], "r");
-    if (src == NULL) {
-        perror("fopen source file");
         exit(EXIT_FAILURE);
     }
 
@@ -28,46 +22,49 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
         // Процесс-сын
-        close(pipefd[1]); // Close write end of pipe
-        dup2(pipefd[0], STDIN_FILENO); // Redirect read end of pipe to stdin
-        close(pipefd[0]); // Close read end of pipe, stdin is now pipe
+        close(fd[0]);
+        if (dup2(fd[1], STDOUT_FILENO) == -1) {
+            perror("dup2");
+            exit(EXIT_FAILURE);
+        }
+
+		FILE *src = fopen(argv[1], "r");
+		if(src == NULL){
+			perror("fopen source file");
+			return EXIT_FAILURE;
+		}
+
+        char line[1000];
+        size_t bytesRead;
+        int cnt = 1;
+        while (fgets(line, sizeof(line), src) != NULL) {
+            if(cnt % 2 == 0){
+                printf("%s", line); // Output for demonstration
+            }
+            cnt++;
+        }
+        fclose(src);
+        close(fd[1]);
+        puts("Son is done");
+        exit(0);
+
+    } else {
+        // Процесс-отец
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
 
         int dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
         if (dest_fd == -1) {
             perror("open destination file");
             exit(EXIT_FAILURE);
         }
-        dup2(dest_fd, STDOUT_FILENO); // Redirect stdout to destination file
-        close(dest_fd); // Close file descriptor, stdout is now destination file
-
+        dup2(dest_fd, STDOUT_FILENO);
+        close(dest_fd);
         execlp("sort", "sort", (char *)NULL);
         // If execlp returns, it's an error
         perror("execlp");
         exit(EXIT_FAILURE);
-    } else {
-        // Процесс-отец
-        close(pipefd[0]); // Close read end of pipe
-        FILE *pipe_stream = fdopen(pipefd[1], "w");
-        if (pipe_stream == NULL) {
-            perror("fdopen");
-            exit(EXIT_FAILURE);
-        }
-
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t nread;
-        int line_number = 0;
-        while ((nread = getline(&line, &len, src)) != -1) {
-            line_number++;
-            if (line_number % 2 == 0) {
-                fputs(line, pipe_stream);
-            }
-        }
-        free(line);
-        fclose(src);
-        fclose(pipe_stream); // Close write end of pipe, triggers EOF for sort
-
-        wait(NULL); // Ждем окончания процесса сына
     }
 
     return EXIT_SUCCESS;
